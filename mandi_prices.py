f953fdae86d0a56dd7a042a091c9601518e93d9f
Jishnu1618 <jishnuroy200316@gmail.com>
2025-08-10 00:17:22 +0530

    Update mandi_prices.py

--- mandi_prices.py
+++ mandi_prices.py
@@ -1,7 +1,7 @@
 import requests
 
-API_KEY = "579b464db66ec23bdd000001fc9a038364fc46e756e42046b7710f83"
-RESOURCE_ID = "cef25fe2-9231-4128-8aec-2c948fedd43f"
+API_KEY = "579b464db66ec23bdd0000158901fc9a038364fc46e756e42046b7710f83"
+RESOURCE_ID = "cef25fe2-9231-4128-8aec-2c948152fedd43f"
 BASE_URL = f"https://api.data.gov.in/resource/{RESOURCE_ID}?api-key={API_KEY}&format=json"
 
 def fetch_mandi_prices(commodity: str, state: str = "", district: str = "", max_records: int = 10):
@@ -17,3 +17,4 @@ def fetch_mandi_prices(commodity: str, state: str = "", district: str = "", max_
 
     # Customize output if needed
     return data.get("records", [])
+
